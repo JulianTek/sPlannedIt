254aa36a49620ab77b12db8a687ba8f0e3b7e5cb
Julian <Julian-Tekstra@hotmail.com>
2020-09-07 14:09:53 +0200

    Made a method for retrieving role names
    This is for registering a new user to instantly pick their role, so admins don't have to go to a separate screen each time a new user is registered

---
+++ sPlannedIt.Data/ConnectionString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace sPlannedIt.Data
+{
+    class ConnectionString : IDisposable
+    {
+        public SqlConnection sqlConnection = new SqlConnection
+        (
+            // local db @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=sPlannedItLocal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+            @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=sPlannedItLocal;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+        );
+
+        public void Dispose()
+        {
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+        }
+    }
+}
---
+++ sPlannedIt.Logic/RoleData_Logic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sPlannedIt.Logic
+{
+    class RoleData_Logic
+    {
+
+        public static List<string> GetRoleNames()
+        {
+            return Data.Role_functions.RoleData_Data.GetRoleNames();
+        }
+    }
+}
